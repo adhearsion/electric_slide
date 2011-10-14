bb3b1b3e7f6d0926d0a9f462520e1f6d0c277adf
Ben Klang <bklang@mojolingo.com>
2011-10-14 14:39:12 -0400

    Initial import of ideas around an automatic call distributor

---
+++ lib/ahn_acd.rb
@@ -0,0 +1,83 @@
+#methods_for :dialplan do
+#end
+#
+#initialization do
+#  COMPONENTS.ahn_acd[:queues].each do |q|
+#    AhnQueue.create q[:name], q[:queue_type], q[:agent_type]
+#  end
+#end
+
+class AhnQueue
+  include Singleton
+
+  def initialize
+    @queues = {}
+  end
+
+  def create(name, queue_type, agent_type = Agent)
+    synchronize do
+      @queues[name] = const_get(queue_type).new unless @queues.has_key?(name)
+      @queues[name].extend agent_type
+    end
+  end
+
+  def get_queue(name)
+    synchronize do
+      @queues[name]
+    end
+  end
+
+  def self.method_missing(method, *args, &block)
+    instance.send method, *args, &block
+  end
+
+  class QueuedCall
+    attr_accessor :call, :queued_time
+
+    def initialize(call)
+      @call = call
+      @queued_time = Time.now
+    end
+
+    def hold
+      call.execute 'StartMusicOnHold'
+      @latch = CountDownLatch.new 1
+      @latch.wait
+      call.execute 'StopMusicOnHold'
+    end
+
+    def make_ready!
+      @latch.countdown!
+    end
+  end
+
+  class Agent
+    def work(agent_call)
+      loop do
+        agent_call.execute 'Bridge', @queue.next_call
+      end
+    end
+  end
+
+  class CalloutAgent
+    def work(agent_channel)
+      @queue.next_call.each do |next_call|
+        next_call.dial agent_channel
+      end
+    end
+  end
+
+  class MeetMeAgent
+    include Agent
+
+    def work(agent_call)
+      loop do
+        agent_call.join agent_conf, @queue.next_call
+      end
+    end
+  end
+
+  class BridgeAgent
+    include Agent
+  end
+end
---
+++ lib/ahn_acd/round_robin.rb
@@ -0,0 +1,35 @@
+require 'thread'
+
+class AhnAcd
+  class RoundRobin
+    include QueueStrategy
+
+    def initialize
+      @queue = []
+      @conditional = ConditionVariable.new
+    end
+
+    def next_call
+      call = nil
+      synchronize do
+        @conditional.wait(@mutex) if @queue.length == 0
+        call = @queue.pop
+      end
+
+      call.make_ready!
+      call
+    end
+
+    # TODO: Add mechanism to add calls with higher priority to the front of the queue.
+
+    def enqueue(call)
+      call = wrap_call(call)
+      synchronize do
+        @queue << call
+        @conditional.signal if @queue.length == 1
+      end
+      super
+    end
+  end
+end
+
