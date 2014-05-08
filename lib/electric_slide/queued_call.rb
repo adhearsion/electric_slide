34a97b526903a4963755de5fc13a058d73ece3e8
Ben Klang <bklang@mojolingo.com>
2014-05-08 19:36:20 -0400

    Queued call proxy object from GAC2014

--- lib/electric_slide/queued_call.rb
+++ lib/electric_slide/queued_call.rb
@@ -1,15 +1,49 @@
 # encoding: utf-8
+require 'countdownlatch'
 
 class ElectricSlide
   class QueuedCall
-    attr_reader :call, :wait_time
+    attr_accessor :call, :queued_time
 
-    def initialize(queue, call)
-      return self if call.is_a? self.class
+    def initialize(call)
+      @call = call
+      @queued_time = Time.now
+    end
 
-      @queue, @call = queue, call
-      @call.auto_hangup = false
-      @wait_time = Time.now
+    def lock
+      @hangup_latch = CountDownLatch.new 1
+      @hangup_latch.wait
+    end
+
+    def free!
+      @hangup_latch.countdown!
+    end
+
+    def hold
+      initiate_moh
+      @latch = CountDownLatch.new 1
+      @latch.wait
+      suspend_moh
+    end
+
+    def make_ready!
+      @latch.countdown!
+    end
+
+    def initiate_moh
+      # TODO: Make MOH configurable
+      @call.execute_controller do
+        moh_handle = play! moh_audio
+        metadata[:moh_handle] = moh_handle
+      end
+    end
+
+    def suspend_moh
+      @call.controllers.each do |controller|
+        if controller.metadata[:moh_handle]
+          controller.metadata[:moh_handle].stop!
+        end
+      end
     end
   end
 end
