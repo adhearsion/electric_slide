f9b197dae02e5574f286713b83cbc17bdda29f83
Ben Langfeld <ben@langfeld.me>
2015-10-02 13:35:28 -0300

    Fail when using deprecated RSpec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ Thread.abort_on_exception = true
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
+  config.raise_errors_for_deprecations!
 
   config.after :each do
     Timecop.return
